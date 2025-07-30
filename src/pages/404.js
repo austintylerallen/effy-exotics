d255119e72e2014954890dbb67ca6a1fcb3f0490
austinallen <austintallen07@gmail.com>
2025-07-29 21:36:48 -0600

    fixed a small next.js issue with the 404.js that was used the wrong way to use a link.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,14 @@
+import Link from "next/link";
+
 export default function NotFound() {
-    return (
-      <main style={{minHeight:'60vh',display:'grid',placeItems:'center',textAlign:'center'}}>
-        <div>
-          <h1>Page not found</h1>
-          <p>Try our <a href="/las-cruces">Las Cruces</a> homepage.</p>
-        </div>
-      </main>
-    );
-  }
-  
+  return (
+    <main style={{ minHeight: '60vh', display: 'grid', placeItems: 'center', textAlign: 'center' }}>
+      <div>
+        <h1>Page not found</h1>
+        <p>
+          Try our <Link href="/las-cruces">Las Cruces</Link> homepage.
+        </p>
+      </div>
+    </main>
+  );
+}
